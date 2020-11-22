22626edee36e0c6fe1713b5b966a5e09aac39924
Sec-li-n <loveuallnbs0205@yahoo.com>
2020-11-22 21:30:11 +0900

    録物件情報のDB設計、モデル、バリデーション

---
+++ app/models/property.rb
@@ -0,0 +1,9 @@
+class Property < ApplicationRecord
+  with_options presence: true do
+    validates :name, length: { maximum: 100 }
+    validates :price, numericality: true
+    validates :address, length: { maximum: 100 }
+    validates :age, numericality: true
+  end
+  validates :note, length: { maximum: 255 }, allow_blank: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_11_22_122400) do
+
+  create_table "properties", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.string "address"
+    t.integer "age"
+    t.string "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
