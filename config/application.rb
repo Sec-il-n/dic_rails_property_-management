adc8fc7ec2c625c6ba285622b8ed9aaf9d56bb05
Sec-li-n <loveuallnbs0205@yahoo.com>
2020-11-22 20:34:48 +0900

    リモートリポジトリ生成と.gitignore修正

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module DevExam
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
+    config.time_zone='Tokyo'
+    config.active_record.default_timezone=:local
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
